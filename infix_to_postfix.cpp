f3db1aa5e8e4c14d17215258c8467ef15c3a42a3
Eeshan Paul <62149967+Eeshan-Paul@users.noreply.github.com>
2021-03-04 20:35:05 +0530

    Add files via upload

---
+++ infix_to_postfix.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int precedence(char a){
+	if (a=='^')
+		return 3;
+	else if (a=='*' || a=='/')
+		return 2;
+	return 1;
+}
+bool isoperator(char a){
+	return (a=='^' || a=='*' || a=='/' || a=='+' || a=='-');
+}
+void infix_to_postfix(string s){
+	stack<char> st;
+	string postfix="";
+	for (int i=0;i<s.length();i++){
+		if (s[i] >= 'a' && s[i] <= 'z')
+			postfix+=s[i];
+		else if (s[i]=='(')
+			st.push(s[i]);
+		else if (s[i]==')'){
+			while (!st.empty() && st.top()!='('){
+				postfix+=st.top();
+				st.pop();
+			}
+			if (st.top()=='(')
+			    st.pop();
+		}
+		else{
+			if (st.empty())
+				st.push(s[i]);
+			else if (precedence(s[i])>precedence(st.top()))
+				st.push(s[i]);
+			else if (precedence(s[i])<=precedence(st.top())){
+				while (!st.empty() && (precedence(s[i])<precedence(st.top()))){
+					postfix+=st.top();
+					st.pop();
+				}
+				st.push(s[i]);
+			}
+	}
+}
+	while (!st.empty()){
+	    postfix+=st.top();
+	    st.pop();
+	}
+	for (int i=0;i<postfix.length();i++)
+		cout<<postfix[i]<<"";
+}
+int main()
+{
+	string s="(a+((b*c)/d)+e)";
+	infix_to_postfix(s);
+	return 0;
+}
