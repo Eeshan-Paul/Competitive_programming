99a49a31d3a04011c8f3aad7b4aa9ad28d7ce0cb
Eeshan Paul <eeshanatri2904@gmail.com>
2023-03-10 01:44:27 +0530

    first commit

--- testing.cpp
+++ testing.cpp
@@ -1,4 +1,4 @@
-    #include<bits/stdc++.h>
+#include<bits/stdc++.h>
     using namespace std;
  
     #define ff              first
@@ -23,8 +23,6 @@
     #define mod             1000000007
     #define sz(x) (int)(x).size()
     mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-    
  
     void setIO(string name = "") {
         if(sz(name)){
@@ -32,47 +30,31 @@
             freopen((name+".out").c_str(), "w", stdout);
         }
     }
-    // void print(int index,int k)
-    // {
-    //     cout<<"Case #"<<index+1<<": "<<k<<endl;
-    // }
-
-    bool check(int ham,int b ,int s,int c,int cb,int cs,int cc,int money,string &str)
-    {
-        int bur=0,sau=0,che=0;
-        rep(i,0,str.length()){if (str[i]=='b') bur++;else if (str[i]=='c') sau++;else che++;}
-        int tbur=(ham * bur) - b;int tsau=(ham * sau) -s;int tche=(ham * che) -c;
-        int total_money=tbur * cb+tsau * cs +tche * cc;
-        return total_money<=money;
-    }
-    
+ 
     
     void solve()
     {   
-        string str;cin>>str;int b,s,c;cin>>b>>s>>c;int cb,cs,cc;cin>>cb>>cs>>cc;int money;cin>>money;
-        int low=1;
-        int high=1e16;
-        int res=0;
-        while (low<=high)
-        {
-            int mid=(low+high);
-            if (check(mid,b,s,c,cb,cs,cc,money,str))
-            {
-                res=mid;
-                low=mid+1;
+            int n,c;cin>>n>>c;
+            int num[n];
+            for(int i=0;i<n;i++){
+               cin>>num[i];
+               num[i]+=i+1;
             }
-            else 
-            {
-                high=mid-1;
+            sort(num,num+n);
+            int i=0;
+            int count=0;
+            while(i<n &&c>=num[i] ){
+                c=c-num[i];
+                count++;
+                i++;
             }
-        }
-        cout<<res<<endl;
+            cout<<(count)<<endl;
     }
  
     int32_t main()
     {
         ios_base::sync_with_stdio(0); cin.tie(0);
-        f(t)
+        f(t) 
         solve();
         return 0;
     }
